9c03c6fa5a71a572ffbded38c6fb27a7af7d8727
ivankp <ivan.pogrebnyak@gmail.com>
2025-05-09 22:53:10 -0400

    File, Record, SubRecord structs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-// use std::mem::size_of;
-
-// struct Record {
-//     data: &[u8],
-//     //    ^ expected named lifetime parameter
-// }
-
 fn as_u32(data: &[u8]) -> u32 {
     let mut x: u32 = 0;
     x += data[3] as u32; x <<= 8;
@@ -20,52 +13,88 @@ fn as_u32(data: &[u8]) -> u32 {
 //     T::from_be_bytes(buf)
 // }
 
-fn parse_subrecords(mut data: &[u8]) -> Vec<&[u8]> {
-    let mut records = Vec::new();
-    while !data.is_empty() {
-        let (head, tail) = data.split_at(as_u32(&data[4..8]) as usize + 8);
-        records.push(head);
-        data = tail;
-    }
-    records
+struct SubRecord {
+    start: u32,
+    size: u32,
 }
 
-fn parse_records(mut data: &[u8]) -> Vec<(&[u8], Vec<&[u8]>)> {
-    let mut records = Vec::new();
-    while !data.is_empty() {
-        let (head, tail) = data.split_at(as_u32(&data[4..8]) as usize + 16);
-        records.push((head, parse_subrecords(&head[16..])));
-        data = tail;
-    }
-    records
-}
-
-fn parse_tes3(data: &[u8]) {
-    println!("Morrowind");
-    let records = parse_records(data);
-    println!("Num records: {:?}", records.len());
-}
-
-fn read_file(path: &String) -> Result<(), Box<dyn std::error::Error>> {
-    let data = std::fs::read(path)?;
-    println!("{:?}", data.len());
-
-    match &data[0..4] {
-        b"TES3" => parse_tes3(&data[..]),
-        b"TES4" => {
-            println!("Oblivion");
-        }
-        _ => {
-            println!("Unexpected file head bytes");
+impl SubRecord {
+    fn new(data: &[u8], start: u32) -> Self {
+        let size = as_u32(&data[4..]) + 8;
+        Self {
+            start: start,
+            size: size,
         }
     }
-    Ok(())
+}
+
+struct Record {
+    start: u32,
+    size: u32,
+    subrecords: Vec<SubRecord>,
+}
+
+impl Record {
+    fn new(data: &[u8], start: u32) -> Self {
+        let size = as_u32(&data[4..]) + 16;
+        let mut subrecords = Vec::new();
+        let mut i: u32 = 16;
+        while i < size {
+            let subrecord = SubRecord::new(&data[i as usize ..], i);
+            i += subrecord.size;
+            // if i > size
+            subrecords.push(subrecord); // TODO: can I create the object in-place?
+        }
+        Self {
+            start: start,
+            size: size,
+            subrecords: subrecords,
+        }
+    }
+}
+
+struct File<'a> {
+    path: &'a str,
+    data: Vec<u8>,
+    records: Vec<Record>,
+}
+
+impl<'a> File<'a> {
+    fn new(path: &'a str) -> Self {
+        let data = std::fs::read(path).unwrap();
+        let size = data.len() as u32; // TODO: check that it fits
+
+        let records = match &data[0..4] {
+            b"TES3" => {
+                let mut records = Vec::new();
+                let mut i: u32 = 0;
+                while i < size {
+                    let record = Record::new(&data[i as usize ..], i);
+                    if i == 0 {
+                        records.reserve_exact(31); // TODO
+                    }
+                    i += record.size;
+                    // if i > size
+                    records.push(record); // TODO: can I create the object in-place?
+                }
+                records
+            }
+            _ => todo!()
+        };
+
+        Self {
+            path: path,
+            data: data,
+            records: records,
+        }
+    }
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let files: Vec<String> = std::env::args().skip(1).collect();
 
-    println!("{:?}", args);
+    println!("{:?}", files);
 
-    read_file(&args[1]).unwrap();
+    let file = File::new(&files[0]);
+    println!("{:?}", file.records.len());
 }
